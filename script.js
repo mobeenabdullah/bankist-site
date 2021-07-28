1df8745d496be90c9a898dfe364ee649f2b49a97
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-07-28 16:32:34 +0500

    added smoth scrolling on all sections in page navigation

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 const nav = document.querySelector('.nav');
+const navLinks = document.querySelectorAll('.nav__links');
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
@@ -36,11 +37,27 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-
+///////////////////////////////////////
 // Smooth Scroll
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
 
   section1.scrollIntoView({ behavior: 'smooth' });
+
+});
+
+///////////////////////////////////////
+// Page Navigation
+
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
 
 });
